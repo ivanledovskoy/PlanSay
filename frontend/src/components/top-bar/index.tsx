cbc21223913862bf345ba6fc721dcd59bb851355
ivanledovskoy <ivan.ledovskoy.708@mail.ru>
2025-04-22 23:29:05 +0300

    [codeDrop] - Рефакторинг кода

--- frontend/src/components/top-bar/index.tsx
+++ frontend/src/components/top-bar/index.tsx
@@ -1,14 +1,25 @@
-import { Box, Grid } from "@mui/material"
+import { StarOutline } from "@mui/icons-material";
+import { Box, Grid, ListItem, ListItemIcon, ListItemText } from "@mui/material"
 import React from "react"
 
-const TopBarComponent = () => {
+const TopBarComponent = (props: any) => {
+  const { title } = props;
   return (
-    <Box display='flex' justifyContent='space-between' px={"32px"} py={"24px"}>
-        <Grid>
-            Welcome!
-        </Grid>
-
-    </Box>
+    <div>
+      <Box display='flex' width='100%' sx = {{borderBottom:`1px solid #3C3C3C`}}>
+        <ListItem>
+            <ListItemIcon>
+              <StarOutline />
+            </ListItemIcon>
+            <ListItemText>
+                <h1>{title}</h1>
+            </ListItemText>
+        </ListItem>
+      </Box>
+      <Box>
+        <h1></h1>
+      </Box>
+    </div>
   )
 };
 
