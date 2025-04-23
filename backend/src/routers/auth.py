c8465280650da8ea3e8d1ab750e2ec732310c788
nullmrpl <marinaplohotnikova33@gmail.com>
2025-04-23 20:59:11 +0300

    [feature] добавлена смена пароля

--- backend/src/routers/auth.py
+++ backend/src/routers/auth.py
@@ -6,6 +6,10 @@ from models.users import User
 from pydantic import BaseModel, EmailStr, Field
 from fastapi.security import HTTPBearer, HTTPAuthorizationCredentials
 from jwt.exceptions import InvalidTokenError
+from sqlalchemy.orm import Session
+from database import get_db
+from crud.users import update_user_info_by_id
+from schemas.users import UserUpdate
 
 
 class TokenInfo(BaseModel):
@@ -34,6 +38,34 @@ def reg_user(creds: UserRegisterSchema):
 
     secret_key = dbUser.getSecretKey()
     return TwoFactorAuth(dbUser.email, secret_key)
+
+http_bearer = HTTPBearer()
+
+def get_current_token_payload(
+        credentials: HTTPAuthorizationCredentials = Depends(http_bearer),
+) -> UserLoginSchema:
+    token = credentials.credentials
+    try:
+        payload = decode_jwt(token=token)
+    except InvalidTokenError as e:
+        raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail=f"Invalid token error: {e}")
+    return payload
+
+def get_current_auth_user(
+        payload: dict = Depends(get_current_token_payload),
+) -> UserLoginSchema:
+    email: EmailStr | None = payload.get("sub")
+    if user := User.getUserByEmail(email):
+        return user
+    raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail="Token invalid (User not found)")
+
+
+def get_current_active_auth_user(
+        dbUser: UserLoginSchema = Depends(get_current_auth_user),
+):
+    if dbUser.active:
+        return dbUser
+    raise HTTPException(status_code=status.HTTP_403_FORBIDDEN, detail="User is not active")
 
 
 router = APIRouter(tags=['Авторизация'])
@@ -81,36 +113,9 @@ def login_user(creds: UserLoginSchema):
 
 
 @router.post("/password-change", summary="Смена пароля")
-def password_change():
-    ...
+def password_change(new_password: str, db: Session = Depends(get_db), user = Depends(get_current_active_auth_user)):
+    return update_user_info_by_id(user.user_id, UserUpdate(password=new_password), db)
 
-http_bearer = HTTPBearer()
-
-def get_current_token_payload(
-        credentials: HTTPAuthorizationCredentials = Depends(http_bearer),
-) -> UserLoginSchema:
-    token = credentials.credentials
-    try:
-        payload = decode_jwt(token=token)
-    except InvalidTokenError as e:
-        raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail=f"Invalid token error: {e}")
-    return payload
-
-def get_current_auth_user(
-        payload: dict = Depends(get_current_token_payload),
-) -> UserLoginSchema:
-    email: EmailStr | None = payload.get("sub")
-    if user := User.getUserByEmail(email):
-        return user
-    raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail="Token invalid (User not found)")
-
-
-def get_current_active_auth_user(
-        dbUser: UserLoginSchema = Depends(get_current_auth_user),
-):
-    if dbUser.active:
-        return dbUser
-    raise HTTPException(status_code=status.HTTP_403_FORBIDDEN, detail="User is not active")
 
 @router.get("/users/me")
 def auth_user_check_self_info(
