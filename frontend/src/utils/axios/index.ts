32db8fe0263cc4a5eba1e71aef79f58dcda1a750
Kseniya <klozowaya@ya.ru>
2025-06-09 19:39:52 +0300

    [fix] - CORS

--- frontend/src/utils/axios/index.ts
+++ frontend/src/utils/axios/index.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export const instance = axios.create({
-  baseURL: 'https://127.0.0.1:8000',
+  baseURL: 'https://158.160.123.223:8000',
   timeout: 1000,
   headers: {'Authorization': `Bearer ${localStorage.getItem('token')}`}
 });
